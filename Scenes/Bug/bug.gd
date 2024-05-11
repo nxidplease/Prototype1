c1041a3c7bd3bfee5cc1e153feb376a0476f6a5d
tomsa9 <48441589+tomsandalon@users.noreply.github.com>
2024-05-11 15:14:35 +0300

    Add movement to enemy

---
+++ Scenes/Bug/bug.gd
@@ -0,0 +1,12 @@
+extends RigidBody2D
+
+const speed = 20
+
+@export var player: Node2D
+@export var nav_agent := $NavigationAgent2D as NavigationAgent2D
+
+func _on_timer_
+
+
+func _on_timer_timeout():
+	pass # Replace with function body.
